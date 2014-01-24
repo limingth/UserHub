c957e003becbaa93b9f066291990b569d1a9787c
Li Ming <limingth@gmail.com>
2014-01-23 19:34:45 -0800

    Create UserHub project and Generate users controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserHub::Application.config.secret_key_base = '87ea45324e5d86511f2cec2a72aa0970b77cc89cf32110bdfd9ff9a2f8f4087633b44dd0212f7a988608a5cf16c7ac10e5eab0b18cea6fcbe8afd91cca517633'
