42a7be5dc7088154d7ad796fad13b538f60eb331
Li Ming <limingth@gmail.com>
2014-01-23 20:59:04 -0800

    Add create user form and show user html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 UserHub::Application.routes.draw do
   get "users/index"
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
